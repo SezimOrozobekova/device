7d204150865000c8fb5125ca7d11a4ad4281dfae
SezimOrozobekova <sezim.orozobekova@alatoo.edu.kg>
2023-09-27 15:57:20 +0600

    List changed to devices

--- src/Main.java
+++ src/Main.java
@@ -21,8 +21,8 @@ public class Main {
         double total_price = 0;
         double total_weight = 0;
 
-        ArrayList<Double> price_list = new ArrayList<>();
-        ArrayList<Double> weight_list = new ArrayList<>();
+        ArrayList<Device> devices = new ArrayList<Device>();
+
 
         for(int i =0; i < quantity; i++){
             int random_number = random.nextInt(30);
@@ -30,25 +30,22 @@ public class Main {
                 Smartphone apple = new Smartphone();
                 apple.setPrice(random.nextDouble(20000));
                 apple.setWeight(random.nextDouble(5));
+                devices.add(apple);
 
-                price_list.add(apple.getPrice());
-                weight_list.add(apple.getWeight());
                 quantity_smartphone += 1;
             } else  if(random_number <= 20){
                 Laptop hp = new Laptop();
                 hp.setPrice(random.nextDouble(20000));
                 hp.setWeight(random.nextDouble(5));
 
-                price_list.add(hp.getPrice());
-                weight_list.add(hp.getWeight());
+                devices.add(hp);
                 quantity_laptop += 1;
             } else {
                 Tablet sumsung = new Tablet();
                 sumsung.setPrice(random.nextDouble(20000));
                 sumsung.setWeight(random.nextDouble(5));
 
-                price_list.add(sumsung.getPrice());
-                weight_list.add(sumsung.getWeight());
+                devices.add(sumsung);
                 quantity_tablet += 1;
             }
 
@@ -56,8 +53,8 @@ public class Main {
         }
 
         for(int j = 0; j < quantity; j++){
-            total_price += price_list.get(j);
-            total_weight += weight_list.get(j);
+            total_price += devices.get(j).getPrice();
+            total_weight += devices.get(j).getWeight();
         }
         System.out.println("Total Price: " + total_price);
         System.out.println("Total weight: " + total_weight + " kg");
